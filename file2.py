e47768a8c6db4ecae6975adefc2d7c7599698ef9
akshat <honeyakshat999@gmail.com>
2019-04-08 05:45:23 -0700

    map

---
+++ file2.py
@@ -0,0 +1,15 @@
+ak=input("plz enter string=")
+newfile="file1.txt"
+second="file3.txt"
+file=open(newfile,'w')
+file.write(ak)
+file.close()
+
+p="file1.txt"
+
+file=open(second,'w')
+file.write()
+file.close()
+
+
+
